1c3b85ab681f09f0032085b95779314faf4d610f
Edoardo Bezzeccheri <coolman3@gmail.com>
2015-01-30 12:36:18 +0100

    Changed license to BSD and renamed folders

--- saveFigs/saveFigs.m
+++ saveFigs/saveFigs.m
@@ -28,19 +28,8 @@ function files = saveFigs(varargin)
 %   the new file will be created with a trailing progressive number, in
 %   order not to overwrite it.
 
-% Copyright 2015 Edoardo Bezzeccheri
-% 
-% Licensed under the Apache License, Version 2.0 (the "License");
-% you may not use this file except in compliance with the License.
-% You may obtain a copy of the License at
-% 
-%    http://www.apache.org/licenses/LICENSE-2.0
-% 
-% Unless required by applicable law or agreed to in writing, software
-% distributed under the License is distributed on an "AS IS" BASIS,
-% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-% See the License for the specific language governing permissions and
-% limitations under the License.
+%   Copyright (c) 2015, Edoardo Bezzeccheri
+%   All rights reserved.
 
 % Initialize the input argument parser
 p = inputParser;
